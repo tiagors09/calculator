6f5da44eb0c9a8a7757aec11babf0753acd8efaf
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-20 16:32:38 -0300

    feat: :sparkles: Add rows with buttons

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -1,3 +1,5 @@
+import 'package:calculator/components/button.dart';
+import 'package:calculator/components/button_row.dart';
 import 'package:flutter/material.dart';
 
 class Keyboard extends StatelessWidget {
@@ -5,14 +7,54 @@ class Keyboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return const SizedBox(
       height: 500,
       child: Column(
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(''),
-          )
+          ButtonRow(
+            buttons: [
+              Button(
+                text: 'AC',
+                big: true,
+              ),
+              Button(text: '%'),
+              Button(text: '/'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '7'),
+              Button(text: '8'),
+              Button(text: '9'),
+              Button(text: 'x'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '4'),
+              Button(text: '5'),
+              Button(text: '6'),
+              Button(text: '-'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(text: '1'),
+              Button(text: '2'),
+              Button(text: '3'),
+              Button(text: '+'),
+            ],
+          ),
+          ButtonRow(
+            buttons: [
+              Button(
+                text: '0',
+                big: true,
+              ),
+              Button(text: ','),
+              Button(text: '='),
+            ],
+          ),
         ],
       ),
     );
