0c8ef4e12e0bdc6c4b300db77c76835c62430c8f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-20 16:20:26 -0300

    feat: :sparkles: Add ButtonRow

---
+++ lib/components/button_row.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'button.dart';
+
+class ButtonRow extends StatelessWidget {
+  final List<Button> buttons;
+
+  const ButtonRow({
+    super.key,
+    required this.buttons,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
