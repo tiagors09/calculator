eca9a09b58feec5e3566d470e6c6e26450a0da91
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-20 16:20:58 -0300

    feat: :sparkles: Add Keyboard

---
+++ lib/components/keyboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Keyboard extends StatelessWidget {
+  const Keyboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(''),
+          )
+        ],
+      ),
+    );
+  }
+}
