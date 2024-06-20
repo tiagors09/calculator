c2367911db23adf4594a123f3a80dcbb7d401a03
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-20 16:32:12 -0300

    feat: :sparkles: Remove radius border and added flag to expand size

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
   final String text;
+  final bool big;
 
   const Button({
     super.key,
     required this.text,
+    this.big = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(text),
+    return Expanded(
+      flex: big ? 2 : 1,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(),
+        ),
+        child: Text(text),
+      ),
     );
   }
 }
