42a80039d9b9433fd1eeaaeab2e5f0f59457db5c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-24 11:31:51 -0300

    feat: :sparkles: Add button colors, color and cb params, named constructors and more style to button

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+
   final String text;
   final bool big;
+  final Color color;
+  final void Function(String) cb;
 
   const Button({
     super.key,
     required this.text,
     this.big = false,
+    this.color = DEFAULT,
+    required this.cb,
+  });
+
+  const Button.big({
+    super.key,
+    required this.text,
+    this.big = true,
+    this.color = DEFAULT,
+    required this.cb,
+  });
+
+  const Button.operation({
+    super.key,
+    required this.text,
+    this.big = false,
+    this.color = OPERATION,
+    required this.cb,
   });
 
   @override
@@ -15,9 +39,15 @@ class Button extends StatelessWidget {
     return Expanded(
       flex: big ? 2 : 1,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () => cb(text),
         style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
           shape: const RoundedRectangleBorder(),
+          backgroundColor: color,
+          textStyle: const TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+          ),
         ),
         child: Text(text),
       ),
