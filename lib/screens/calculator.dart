d91a84c9d25815ac2198dc6956244f0171b5c5b6
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-24 11:33:07 -0300

    feat: :sparkles: Change Calculator to Stateful widget, add onPressed fn and set orientation

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,17 +1,43 @@
 import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Column(
         children: [
-          Display(text: '123'),
-          Keyboard(),
+          Display(text: memory.value),
+          Keyboard(
+            cb: _onPressed,
+          ),
         ],
       ),
     );
